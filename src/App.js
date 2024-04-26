46447dffac9841a63678d8172066e4dbd1992b35
Roshini <roshinianeedha@gmail.com>
2024-04-26 08:45:18 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+//import React, { useEffect } from 'react';
 import './App.css';
 import styled, { ThemeProvider } from 'styled-components';
 import { darkTheme } from './utils/Themes';
@@ -11,6 +11,8 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+
+//import logo from './images/portfolio.svg';
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -26,9 +28,7 @@ const Wrapper = styled.div`
 `
 
   function App() {
-    useEffect(() => {
-      document.title = "Your Page Title"; // Set your desired title here
-    }, []);
+    
 
     
   return (
